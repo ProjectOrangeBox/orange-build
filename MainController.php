83f24aaad430934f6f1138aa7e590651c42ff585
Don Myers <donmyers@quadratec.com>
2018-05-18 16:57:21 -0400

    updates

---
+++ MainController.php
@@ -0,0 +1,9 @@
+<?php
+
+class MainController extends MY_Controller {
+
+	public function indexAction() {
+		ci('page')->render();
+	}
+
+} /* end class */
---
+++ index_view.php
@@ -0,0 +1,7 @@
+<? pear::extends('_templates/orange_default') ?>
+
+<? pear::section('section_container') ?>
+
+<h1>Welcome!</h1>
+
+<? pear::end() ?>
